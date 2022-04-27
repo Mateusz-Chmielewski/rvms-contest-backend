1758dc041e2daa1447f93437b417badb09df0da3
Mateusz Chmielewski <mateusz.chmielewski1@protonmail.com>
2022-04-27 10:18:53 +0200

    Add hashing of login data

--- src/main/java/pl/edu/zsel/contestbackend/authentication/model/AdminUser.java
+++ src/main/java/pl/edu/zsel/contestbackend/authentication/model/AdminUser.java
@@ -1,16 +1,24 @@
 package pl.edu.zsel.contestbackend.authentication.model;
 
-import lombok.AllArgsConstructor;
+import com.google.common.hash.Hashing;
 import lombok.NoArgsConstructor;
-import lombok.Value;
+import org.springframework.aop.scope.ScopedProxyUtils;
 
-@Value
+import java.nio.charset.StandardCharsets;
+
 @NoArgsConstructor
 public class AdminUser {
     private final String username = "admin";
     private final String password = "ZAQ!2wsx";
 
     public boolean isAdmin(User user) {
-        return user.getUsername().equals(username) && user.getPassword().equals(password);
+        return user.getUsername().equals(getHash(username))
+                && user.getPassword().equals(getHash(password));
+    }
+
+    private String getHash(String string) {
+        return Hashing.sha256()
+                .hashString(string, StandardCharsets.UTF_8)
+                .toString();
     }
 }
